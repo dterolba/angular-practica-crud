8fc3a9e45c80206aff7f825e61f0b37b203348d6
dterolba <dterolba@emeal.nttdata.com>
2025-02-26 10:30:27 +0100

    FEAT: se crea el componente de creación

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CarDetailsComponent } from './home/car-details/car-details.component';
+import { NewCarComponent } from './home/new-car/new-car.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
+    { path: 'cars/new', component: NewCarComponent},
     { path: 'cars/:carID', component: CarDetailsComponent},
     { path: '', redirectTo: 'home', pathMatch:'full'}
 ];
---
+++ frontend/src/app/home/new-car/new-car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'app-new-car',
+  imports: [],
+  templateUrl: './new-car.component.html',
+  styleUrl: './new-car.component.css'
+})
+export class NewCarComponent {
+
+}
